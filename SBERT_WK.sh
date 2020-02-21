170777b70a261fde9bb301a05fca434a45a1df70
Bin Wang <bwang28c@gmail.com>
2020-02-21 11:36:15 -0800

    Add files via upload

--- SBERT_WK.sh
+++ SBERT_WK.sh
@@ -2,12 +2,12 @@
 
 
 args=(
-    --model_type 'binwang/bert-base-nli'
-    --embed_method 'dissecting'    		
+    --model_type 'binwang/bert-base-uncased'
+    --embed_method 'dissecting'     # ave_last_hidden    dissecting		
     --max_seq_length 128                # Truncate if exceeding max length
     --batch_size 64                     # Batch size for extracting sentence features
     --context_window_size 2             # Window size for Alignment and Novelty
-    --tasks 'sts'
+    --tasks 'all'
     )
 
 echo "${args[@]}"
